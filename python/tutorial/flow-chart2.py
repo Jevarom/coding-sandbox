9dd5d7e6a98d03d99c047e428fb7a8423a8b61c6
unknown <Jevarom>
2022-12-24 23:57:51 -0800

    Work on while loops

---
+++ python/tutorial/flow-chart2.py
@@ -0,0 +1,15 @@
+spam = 0
+while spam <5:
+    print("Hello, world.")
+    spam = spam + 1
+
+while True:
+    print('Who are you?')
+    name = input()
+    if name != 'Joe':
+        continue
+    print('Hello, Joe. What is the password? (It is a fish.)')
+    password = input()
+    if password == 'swordfish':
+        break
+print('Access granted.')
